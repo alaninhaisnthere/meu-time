b94df9d34233f01997a0e77460fc916e9920d16d
Alana <alanaoliveira71@gmail.com>
2023-05-26 15:08:46 -0300

    implementando lógica de geração de gráficos

---
+++ src/utils/useGraphics.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+interface GraphicsData {
+  time: string;
+  percentage: number;
+}
+
+export function useGraphics() {
+  const [graphicsData, setGraphicsData] = useState<GraphicsData[]>([]);
+
+  useEffect(() => {
+    const generateRandomData = () => {
+      const data: GraphicsData[] = [
+        { time: '0-15', percentage: Math.random() * 100 },
+        { time: '16-30', percentage: Math.random() * 100 },
+        { time: '31-45', percentage: Math.random() * 100 },
+      ];
+      setGraphicsData(data);
+    };
+
+    generateRandomData();
+  }, []);
+
+  return graphicsData;
+}
